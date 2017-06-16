ef91b421dce1e912c321d044f8b229ec6e4eca5b
Diana Huang <dkh@edx.org>
2017-06-16 15:07:24 -0400

    Use AjaxHelpers instead of mock-ajax.

--- openedx/features/course_experience/static/course_experience/js/spec/WelcomeMessage_spec.js
+++ openedx/features/course_experience/static/course_experience/js/spec/WelcomeMessage_spec.js
@@ -1,27 +1,25 @@
 /* globals $, loadFixtures */
 
-import 'mock-ajax';
 import { WelcomeMessage } from '../WelcomeMessage';
+import { expectRequest } from 'edx-ui-toolkit/js/utils/spec-helpers/ajax-helpers';
 
 describe('Welcome Message factory', () => {
   describe('Ensure button click', () => {
     const endpointUrl = '/course/course_id/dismiss_message/';
     beforeEach(() => {
-      jasmine.Ajax.install();
       loadFixtures('course_experience/fixtures/welcome-message-fragment.html');
       WelcomeMessage(endpointUrl);
-    });
-    afterEach(() => {
-      jasmine.Ajax.uninstall();
     });
     it('When button click is made, ajax call is made and message is hidden.', () => {
       const message = document.querySelector('.welcome-message');
       document.querySelector('.dismiss-message button').dispatchEvent(new Event('click'));
-      expect(jasmine.Ajax.requests.mostRecent().url).toBe(endpointUrl);
-      jasmine.Ajax.requests.mostRecent().respondWith({
-        status: 200,
-        contentType: 'text/plain',
-      });
+      requests = AjaxHelpers.requests(this);
+      AjaxHelpers.expectRequest(
+        requests,
+        'POST',
+        endpointUrl
+      );
+      AjaxHelpers.respondWithJson(requests);
       expect(message.innerHtml).toBe('');
     });
   });
